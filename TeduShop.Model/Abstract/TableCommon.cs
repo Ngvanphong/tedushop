cc19491b27f79995aa2b2f0827c7b54ab8e8e3e1
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-11-19 11:06:39 +0700

    11AM1911

---
+++ TeduShop.Model/Abstract/TableCommon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public class TableCommon : ISeotable, IAuditable, ISwitchtable
+    {
+        public string MetaKeyword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string MetaDiscription { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreateDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CreateBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string UpdatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ TeduShop.Model/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+    public class Product: TableCommon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Alias { set; get; }
+        [Required]
+        public int CategoryID { set; get; }
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { set; get; }
+        public int? DisplayOrder { set; get; }
+        public string Image { set; get; }
+        public XElement MoreImage { set; get; }
+        public Decimal Price { get; set; }
+        public Decimal? PromotionPrice { set; get; }
+        public int? Warranty { set; get; }
+        public string Description { set; get; }
+        public string Content { set; get; }
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { set; get; }
+    }
+}
