d0cfad53442de66c58413946ed87a37f7e3e274d
DESKTOP-2KTJBE5\NGUYEN VAN PHONG <ngvanphong2012@gmail.com>
2017-11-24 23:07:28 +0700

    11h24/11

---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using TeduShop.Data.Inframestructure;
+using TeduShop.Data.Reponsitories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _uniOfWork;
+        private IPostCategoryService _postCategoryService;
+        private List<PostCategory> _listPostCategory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _uniOfWork = new Mock<IUnitOfWork>();
+            _postCategoryService = new PostCategoryService(_mockRepository.Object, _uniOfWork.Object);
+            _listPostCategory = new List<PostCategory>()
+            {
+                new PostCategory(){ID=1,Name="P1",Alias="p1",Status=true},
+                new PostCategory(){ID=2,Name="P2",Alias="p2",Status=true},
+                new PostCategory(){ID=3,Name="P3",Alias="p3",Status=true},
+            };
+        }
+
+        [TestMethod]
+        public void PostCategoryService_GetAll()
+        {
+            //setup method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_listPostCategory);
+            //call method
+            var result = _postCategoryService.GetAll() as List<PostCategory>;
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void PostCategoryService_Create()
+        {
+        }
+    }
+}
