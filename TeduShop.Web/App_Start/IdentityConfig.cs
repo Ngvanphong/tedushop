f6c70ca78a9470ad135936dd45d053907f181c5c
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-04-07 00:04:22 +0700

    12h06/04

--- TeduShop.Web/App_Start/IdentityConfig.cs
+++ TeduShop.Web/App_Start/IdentityConfig.cs
@@ -51,10 +51,10 @@ namespace TeduShop.Web.App_Start
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
