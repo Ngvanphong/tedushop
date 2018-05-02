1a3cca7edb46d40911da1d804bcb92d584577469
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-05-02 21:07:11 +0700

    180502

--- TeduShop.Web/App_Start/BundleConfig.cs
+++ TeduShop.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace TeduShop.Web
 {
@@ -8,24 +7,40 @@ namespace TeduShop.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                        "~/Assets/client/js/jquery-1.11.0.min.js"
+                        ));
+            bundles.Add(new ScriptBundle("~/js/plugins").Include(
+                       "~/Scripts/jquery-ui-1.12.1.min.js",
+                       "~/Scripts/jquery.validate.min.js",
+                       "~/Scripts/mustache.js",
+                       "~/Scripts/numeral/numeral.min.js",
+                       "~/Assets/client/js/bootstrap.min.js",
+                       "~/Assets/client/js/common.js",
+                       "~/Assets/client/js/jquery.cookie.js",
+                       "~/Assets/client/js/waypoints.min.js",
+                       "~/Assets/client/js/modernizr.js",
+                       "~/Assets/client/js/bootstrap-hover-dropdown.js",
+                       "~/Assets/client/js/owl.carousel.min.js",
+                       "~/Assets/client/js/front.js"
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                       ));
+            bundles.Add(new ScriptBundle("~/js/shoppingcart").Include(
+                       "~/Assets/client/js/shoppingCart.js"
+                       ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css/base")
+                .Include("~/Assets/client/css/font-awesome.css",new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/bootstrap.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/themes/base/jquery-ui.min.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/animate.min.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/owl.carousel.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/owl.theme.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/style.default.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/custom.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/customerRe.css", new CssRewriteUrlTransform())
+                     );
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
